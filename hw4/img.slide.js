9df6de600ca265f26799083cb0c02b8b2187121c
zmontgom <zangoochi0@gmail.com>
2014-12-02 23:52:45 -0500

    Finished HW4

--- hw4/img.slide.js
+++ hw4/img.slide.js
@@ -2,15 +2,37 @@
 //===DEFAULT VALUES============================================================================================
 
 //Array of images
-var pic = [ "pictures/car1.jpeg", "pictures/car2.jpeg",
-     "pictures/car3.jpeg", "pictures/car4.jpeg", "pictures/car5.jpeg",
-     "pictures/car6.jpeg", "pictures/car7.jpeg", "pictures/car8.jpeg",
-     "pictures/car9.jpeg", "pictures/car10.jpeg", "pictures/car11.jpeg", "pictures/car12.jpeg"];
+var pic = [ 
+			"pictures/car1.jpg", 
+			"pictures/car2.jpg",
+     		"pictures/car3.jpg", 
+     		"pictures/car4.jpg", 
+     		"pictures/car5.jpg",
+     		"pictures/car6.jpg", 
+     		"pictures/car7.jpg", 
+     		"pictures/car8.jpg",
+     		"pictures/car9.jpg", 
+     		"pictures/car10.jpg", 
+     		"pictures/car11.jpg", 
+     		"pictures/car12.jpg"];
 
 
 //Captions for the images. 
 //PRE: Indexes must match the index of the picture in pic
-var captions = ["car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9", "car10", "car11", "car12"];
+var captions = [
+				"1913 Helica", 
+				"1905 Rolls Royce", 
+				"1937 Bugatti 57S", 
+				"1937 Horch 853 Voll & Ruhrbech Sport Cabriolet", 
+				"1931 Packard 883 Sport Phaeton", 
+				"1948 Aston Martin DB1", 
+				"1954 Oldsmobile F-88", 
+				"1954 Jaguar D-Type", 
+				"1957 Ferrari 250 Testa Rossa", 
+				"1972 Porshe 916", 
+				"1962 Ferrari 250 GTO", 
+				"1948 Tucker Sedan"
+				];
 
 //Slide of all the pictures
 var slide = new Array();
@@ -32,13 +54,16 @@ var forward_icon = "pictures/forward.png";
 var back_icon = "pictures/backward.png";
 
 //Number of tiles to be displayed on the page.
-var number_of_tiles = 4;
+var number_of_tiles = 3;
 
 
 //Current displayed image height and width
-var width = 280;
+var picture_width = 280;
+var picture_height = 280;
 
-var height = 280;
+//Tile size
+var tile_width = 100;
+var tile_height = 100;
 
 //============================================================================================================
 
@@ -53,14 +78,23 @@ if ( document.images ){
 }
 
 //preloading all styles such as background color and icons
-setTiles(index);
+check_next(index);
 document.getElementById("display").src= slide[Math.floor((Math.random() * pic.length-1) + 1)].src; 
-document.getElementById("display").width = width;
-document.getElementById("display").height = height;
+document.getElementById("display").width = picture_width;
+document.getElementById("display").height = picture_height;
 document.getElementById('display').style.background = default_background_color;
 document.getElementById('tiles').style.background = default_background_color;
 document.getElementById('forward_btn').src = forward_icon;
 document.getElementById('back_btn').src = back_icon;
+
+var tile_size = document.getElementsByClassName('tiles');
+for(var i in tile_size){
+	tile_size[i].width = tile_width;
+	tile_size[i].height = tile_height;
+}
+
+//Preloading addEventListeners for the captions for mouseover and mouseout
+addListener();
 
 //============================================================================================================
 
@@ -72,6 +106,9 @@ function loadImage(url)
      {
          rslt = new Image();
          rslt.src = url; 
+         rslt.id = url;
+         rslt.setAttribute('class', 'tiles');
+		 rslt.setAttribute('onClick', 'reply_click(this.id)');
          return rslt; 
       }
 }
@@ -80,108 +117,53 @@ function loadImage(url)
 
 //===HELPER METHODS===========================================================================================
 
-//changes Slide the the current index
-//Also toggles which pic is selected in the tiles
-function changeSlide()
-{ 
-	
-	toggle_selected(pic[index]);
-	
-
-
+function nextSlide(start)
+{
+	var value = tiles[number_of_tiles-1];
+	start = slide.indexOf(value);
+	check_next(start); 
 }
 
-
-//Sets index to a particular value
-//Then changes slide to that index
-//Used for choosing a tile to be displayed
-function setSlide(value)
+function check_next(start)
 {
-	index = value;
-	changeSlide();
-
-}
-
-
-//Arrow controls he previous displayed. 
-//Shifts tiles to show 1 less. 
-function prevSlide()
-{
-
-	var idx = --index;
- 	if(idx < 0) 
- 	{ 
- 		++index;
- 		return 
- 	}
-
- 	if(idx+1 > 0 && idx < slide.length-(number_of_tiles-1) && tiles[0] == slide[idx+1])
- 	{
- 		setTiles(index);	
-	}
-	changeSlide(); 
-}
-
-function nextSlide()
-{
-	var idx = ++index;
-   if( idx >= pic.length) 
-   	{ 
-   		--index;
-   		return; 
-   	}
- 
-   	if(idx < slide.length && tiles[number_of_tiles-1] == slide[idx-1])
-   	{
-   		set_next(idx);
-   		tile_count = 0;
-	}
-	changeSlide(); 
-}
-
-
-//Sets the tiles. Tiles depend on the number_of_tiles
-function setTiles(value)
-{
-	if(value == null)
-	{
-		value = 0;
-	}
-	document.getElementById('tiles').innerHTML = "";
-	tiles = [];
-	for (var i in slide){
-		if(i < number_of_tiles){
-		var img = create_image(value);
-
-		tiles.push(img);
-		document.getElementById('tiles').appendChild(img);
-		}
-		else
+	if(slide.length-start >= number_of_tiles){
+		tiles = [];
+		document.getElementById('tiles').innerHTML = "";
+		for(var i=0, j=start; i<number_of_tiles; ++i, ++j)
 		{
-			break;
+			tiles[i] = slide[j];
+			document.getElementById('tiles').appendChild(tiles[i]);
 		}
-		++value;
+		addListener();
+	}else{
+			check_next(start-1);
 	}
 }
 
-function set_next(value){
+function prevSlide(start)
+{
+	var value = tiles[0];
+	start = slide.indexOf(value);
+	check_previous(start); 
+}
 
-	//Swap function to shift tiles to the right
-	var temp = [];
-	for(var i=0; i<number_of_tiles-1; ++i){
-		temp[i] = tiles[i+1]
-	}
-	//Create the new image for the last element
-	var img = create_image(value);
-	temp[number_of_tiles-1] = img;
-
-	document.getElementById('tiles').innerHTML = "";
-	tiles = [];
-
-	for (var j=0; j<temp.length; ++j){
-		var img = temp[j];
-		tiles.push(img);
-		document.getElementById('tiles').appendChild(img);
+function check_previous(start)
+{
+	if(start > 0)
+	{
+		document.getElementById('tiles').innerHTML = "";
+		if(slide[start-(number_of_tiles-1)])
+		{
+			start = start-(number_of_tiles-1);
+			for(var i=0, j=start; i<number_of_tiles; ++i, ++j)
+			{
+				tiles[i] = slide[j];
+				document.getElementById('tiles').appendChild(tiles[i]);
+			}
+			addListener();
+		}else{
+			check_previous(start+1);
+		}
 	}
 }
 
@@ -199,14 +181,38 @@ function create_image(value)
 function reply_click(value)
 {
 	var i = pic.indexOf(value);
-	document.getElementById("display").src= slide[i].src; 
-	document.getElementById("display").width = width;
-	document.getElementById("display").height = height;
+	var display = document.getElementById("display");
+	display.src= slide[i].src; 
+	display.width = width;
+	display.height = height;	
+
 	toggle_selected(value);
-	var capt = document.getElementById('caption')
+
+	var capt = document.getElementById('caption');
 	capt.className = "tooltip";
 	capt.innerHTML = captions[i];
-	setSlide(i);
+
+}
+
+function addListener(){
+	var tile = document.getElementsByClassName("tiles");
+
+    for(var i = 0; i<tile.length; i++){
+
+       	tile[i].addEventListener("mouseover", function() 
+       	{ 
+       		var capt = document.getElementById('tooltip-container');
+			capt.className = "tooltip";
+			capt.innerHTML = captions[pic.indexOf(this.id)]; 
+		});
+
+        tile[i].addEventListener("mouseout", function() 
+        { 
+        	var capt = document.getElementById('tooltip-container');
+        	capt.className = ""; 
+        	capt.innerHTML = "";
+		});
+   	}
 }
 
 function toggle_selected(value)
@@ -220,25 +226,9 @@ function toggle_selected(value)
 		if(images[i].id == value)
 		{
 			images[i].className = "tiles selected";
-
 		}
 	}
 }
-
-var tile = document.getElementsByClassName("tiles");
-
-    for(var i = 0, j=tile.length; i<j; i++){
-
-        tile[i].addEventListener("mouseover", function() { var capt = document.getElementById('tooltip-container');
-									capt.className = "tooltip";
-									capt.innerHTML = captions[pic.indexOf(this.id)]; 
-								});
-        tile[i].addEventListener("mouseout", function() { var capt = document.getElementById('tooltip-container');
-        							capt.className = ""; 
-        							capt.innerHTML = "";
-								});
-   
-    }
 
 //============================================================================================================
 
